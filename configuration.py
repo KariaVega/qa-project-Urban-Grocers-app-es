2fcba85e4a5dd8d413cc8b0e2e694bda94d8e8cf
Tlalpan@karinavega <arqkarvga@gmail.com>
2024-05-29 18:37:17 -0600

    Actualizacion de URL 3

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-71cf33f8-96ad-4167-a2fb-3a8c605c1f53.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-c01a2c89-cd99-4220-a590-f5a9209aa1e6.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"
 CREATE_KITS_PATH = "/api/v1/kits/"
